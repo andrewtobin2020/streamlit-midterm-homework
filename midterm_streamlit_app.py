9fabfb076eca2fbf7006e2badfe7ebba86b4fd9f
andrewtobin2020 <71295562+andrewtobin2020@users.noreply.github.com>
2022-11-07 10:59:00 -0500

    Add files via upload

---
+++ midterm_streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import plotly.express as px
+import numpy as np
+import pandas as pd
+import altair as alt
+
+data = pd.read_csv("nfl_mdf.csv")
+st.title ("NFL Games Streamlit App")
+scatter = alt.Chart(data, title = "Wind vs Total Score since 2000").mark_point().encode(
+    x = 'wind',
+    y='total'
+    #tooltip = ['x','y']
+)
+st.altair_chart(scatter, use_container_width=True)
+
+teams = data["home_team"].sort_values().unique()
+team = st.selectbox('Pick your favorite team',teams) 
+st.text_area(f'Why is {team} your favorite team?')
+
+team2 = st.selectbox('Pick your least favorite team',teams) 
+st.text_area(f'Why is {team2} your least favorite team?')
+
+st.text_area('If you are playing fantasy football, how is your team doing? (This is a safe space)')
